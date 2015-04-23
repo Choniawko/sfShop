312eae83d39d4f006bcdce28c0590f12fa7ef368
Choniawko <pawel.choniawko@gmail.com>
2015-04-23 14:32:12 +0200

    widok zamówień

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -6,21 +6,21 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-use AppBundle\Entity\Order;
+use AppBundle\Entity\Orders;
 
-/**
- * @Route("zamowienia", name="orders_list")
- */
+
  class OrderController extends Controller
  {
+ 	/**
+ * @Route("/zamowienia", name="orders_list")
+ */
     public function indexAction()
     {
-    	$orders = $this
-    	             ->getUser()
-    	             ->getOrders();
 
-        return $this->render('orders/index.html.twig', [
-            'orders' => $orders
-        ]);
+        $orders = $this->getUser()->getOrders();
+
+         return $this->render('orders/index.html.twig', [
+             'orders' => $orders,
+         ]);
     }
  } 
